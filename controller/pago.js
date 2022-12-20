b625767abfc4fd1cd476cebbde0d0ba2863942c9
tinchomaidana22 <martinmaidana90@gmail.com>
2022-12-19 21:09:12 -0300

    corrijo detalles env

--- controller/pago.js
+++ controller/pago.js
@@ -2,7 +2,7 @@ const mercadopago = require('mercadopago')
 
 // Agrega credenciales (ACCESS TOKEN - Del vendedor)
 mercadopago.configure({
-    access_token: 'TEST-5840650845306044-120119-8b1aa2a398102c63f5dd40fde4f8586b-1252941424',
+    access_token: `${process.env.TOKEN}`,
 });
 
 console.log('----- configuración de SDK de mercadopago ok! -----')
